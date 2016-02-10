a13a475a919602c10643363bf78927242dd0d6f7
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2016-02-09 22:42:59 -0300

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
 import NLP.Summarizer
 
+import Control.Monad
+import qualified Data.Text.IO as TIO
+import Options.Generic
+
+data CliOptions = CliOptions
+  { ratio :: Maybe Int <?> "summarization % [default = 20%]"
+  , dictionary :: Maybe String <?> "dictionary to use"
+  , input :: Text <?> "text to summarize"
+  , output :: Maybe FilePath <?> "output file [default = stdout]"
+  , html :: Bool <?> "output as html"
+  , keywords :: Bool <?> "only output keywords"
+  , about :: Bool <?> "only output the summary"
+  , version :: Bool <?> "show version information"
+  } deriving (Generic, Show)
+
+instance ParseRecord CliOptions
+
 main :: IO ()
-main = someFunc
+main = do
+  x <- (getRecord "Test program" :: IO CliOptions)
+
+  let txt = unHelpful (input x)
+
+  -- let txt = "Ojalá esa comisión cumpla con su deber. Hoy en día no prosperan los negocios pequeños en Argentina gracias a los hermosos oligopolios que se han armado las grandes empresas, que les encanta aprovecharse de su posición.\nSí, las estoy mirando a ustedes telefónicas, si no fuera por ustedes tendría 300mb de internet hasta en Jujuy."
+  TIO.putStrLn txt
+
+  result <- summarize "es" txt
+  forM_ (summarySentences result) TIO.putStrLn
+  forM_ (summaryKeywords result) TIO.putStrLn
+
+  pure ()
--- src/NLP/Summarizer.hs
+++ src/NLP/Summarizer.hs
@@ -1,6 +1,34 @@
 module NLP.Summarizer
-  ( someFunc )
+  ( summarize
+  , SummarizerResult(..))
   where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import NLP.Summarizer.Arguments
+import NLP.Summarizer.Article
+import NLP.Summarizer.Dictionary
+import NLP.Summarizer.Grader
+import NLP.Summarizer.Highlighter
+import NLP.Summarizer.Sentence
+
+import Data.Text (Text)
+import qualified Data.Text as T
+
+data SummarizerResult = SummarizerResult
+  { summarySentences :: [Text]
+  , summaryKeywords :: [Text]
+  } deriving Show
+
+summarize :: Text -- ^ dictionary to use (language)
+          -> Text -- ^ text to summarize
+          -> IO SummarizerResult
+summarize lang txt = do
+  dict <- loadFromFile (T.unpack lang)
+  let args = SummarizerArguments { dictionaryLanguage = lang
+                                 , inputString = txt
+                                 , ratio = RatioByPercent 10
+                                 }
+      art = (highlight args . grade . parseText txt . createArticle) dict
+  pure SummarizerResult { summarySentences =
+                            map originalSentence (sentences art)
+                        , summaryKeywords = concepts art
+                        }

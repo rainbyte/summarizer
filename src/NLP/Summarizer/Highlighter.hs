a76080af4827a9fcf69041e36cc3f260bf02ab9d
Alvaro Fernando Garcia <alvarofernandogarcia@gmail.com>
2016-05-31 11:29:01 -0300

    Fix sentence sort down by score, giving priority to first sentence, see details...
    This change:
    - Preserves stability, which means keeping already ordered sentences untouched.
    - Order sentences from left to right, picking the first when scores are the same.
    - Behaves like Grader, which also gives higher score to first sentence.
    
    Warning! this behaviour gives different results from original OTS

--- src/NLP/Summarizer/Highlighter.hs
+++ src/NLP/Summarizer/Highlighter.hs
@@ -4,7 +4,8 @@ import NLP.Summarizer.Article
 import NLP.Summarizer.Arguments
 import NLP.Summarizer.Sentence (score, selected, terms, wordCount)
 
-import Data.List (sortOn)
+import Data.List (foldl', sortOn)
+import Data.Ord (Down(..))
 
 highlight :: SummarizerArguments -> Article -> Article
 highlight args =
@@ -15,19 +16,19 @@ highlight args =
 
 selectSentencesByPercent :: Int -> Article -> Article
 selectSentencesByPercent percent article =
-  let sentencesByScore = sortOn score (sentences article)
+  let sentencesByScore = sortOn (Down . score) (sentences article)
       totalWords = foldr (\s r -> length(terms s) + r) 0 (sentences article)
       maxWords = truncate ( fromIntegral totalWords
                           * fromIntegral percent / 100)
-      f s (r, n) = if n >= maxWords
+      f (r, n) s = if n >= maxWords
                    then (r, n)
-                   else (s { selected = True } : r, n + wordCount s)
-      (sentences', _) = foldr f ([], 0) sentencesByScore
+                   else (r ++ [s { selected = True }], n + wordCount s)
+      (sentences', _) = foldl' f ([], 0) sentencesByScore
   in article { sentences = sentences' }
 
 selectNumberOfSentences :: Int -> Article -> Article
 selectNumberOfSentences lineCount article =
-  let sentencesByScore = reverse (sortOn score (sentences article))
+  let sentencesByScore = sortOn (Down .score) (sentences article)
       (xs,ys) = splitAt lineCount sentencesByScore
       xs' = map (\s -> s { selected = True }) xs -- TODO: filter empty from xs
   in article { sentences = xs' ++ ys }
